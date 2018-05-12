1c0f954e7ba199a9da9ed35506b78de9d8dbb2de
Teddy <gedu07@gmail.com>
2018-05-12 20:37:26 -0300

    Initial commit from cap 3 of codelab

---
+++ mdc_100_series/lib/home.dart
@@ -0,0 +1,98 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'supplemental/asymmetric_view.dart';
+
+import 'model/data.dart';
+import 'model/product.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        title: Text("SHRINE"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: AsymmetricView(products: getProducts(Category.all),),
+    );
+  }
+
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = getProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData data = Theme.of(context);
+    final NumberFormat numberFormat = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString(),
+    );
+
+    return products.map((product) {
+      return Card(
+        elevation: 0.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                fit: BoxFit.fitWidth,
+                package: product.assetPackage,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(product.name,
+                    style: data.textTheme.button,
+                    maxLines: 1,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(numberFormat.format(product.price),
+                    style: data.textTheme.caption,),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+}
